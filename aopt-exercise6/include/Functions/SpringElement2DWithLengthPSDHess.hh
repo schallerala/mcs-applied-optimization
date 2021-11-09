089e61a13f389882a6142876fa18730daa77ed37
Alain Schaller <schallerala@gmail.com>
2021-11-09 21:52:05 +0100

    Solve bonus exercise - but not sure if needs to change Newton method

--- aopt-exercise6/include/Functions/SpringElement2DWithLengthPSDHess.hh
+++ aopt-exercise6/include/Functions/SpringElement2DWithLengthPSDHess.hh
@@ -20,16 +20,37 @@ namespace AOPT {
         SpringElement2DWithLengthPSDHess() : SpringElement2DWithLength() {}
 
         inline virtual void eval_hessian(const Vec &_x, const Vec &_coeffs, Mat &_H) override {
+            SpringElement2DWithLength::eval_hessian(_x, _coeffs, _H);
+
+            // slide 18 and http://eigen.tuxfamily.org/dox/classEigen_1_1SelfAdjointEigenSolver.html
             //------------------------------------------------------//
-            //TODO: compute the hessian matrix and project it to a positve definite matrix
+            // compute the hessian matrix and project it to a positve definite matrix
             //Hint: 1. to compute the eigen decomposition, use
             //          Eigen::SelfAdjointEigenSolver<Mat> solver(A);
+            Eigen::SelfAdjointEigenSolver<Mat> solver(_H);
+
             //          Mat evecs = solver.eigenvectors();  //this matrix contains the eigenvectors in its columns
             //          Vec evals = solver.eigenvalues();
             //      2. to convert a vector d to a (dense) diagonal matrix D, use
             //          D = d.asDiagonal()
+            const Vec D = solver.eigenvalues();
+            const auto Q = solver.eigenvectors();
 
+            Vec new_m_i(D.size());
 
+            // M = Q^T * diag(m_i) * Q
+            //        +- 0,                     lambda_i >= epsilon
+            // m_i = <
+            //        +- epsilon - lambda_i,    lambda_i < epsilon
+            for (size_t i = 0; i < D.size(); ++i) {
+                const auto lambda_i = D[i];
+                const double m_i = lambda_i >= m_eps
+                             ? 0
+                             : m_eps - lambda_i;
+                new_m_i[i] = m_i;
+            }
+
+            _H = Q.transpose() * new_m_i.asDiagonal() * Q;
 
             //------------------------------------------------------//
         }
