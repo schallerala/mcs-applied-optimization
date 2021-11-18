eea6f257a2b440bb83af881475055ea2790f005f
Alain Schaller <schallerala@gmail.com>
2021-11-18 10:29:37 +0100

    Initial commit after unzip

---
+++ aopt-exercise8/MassSpringSystem/MassSpringSystemT_impl.hh
@@ -0,0 +1,184 @@
+#define MASSSPRINGSYSTEM_C
+
+#include "MassSpringSystemT.hh"
+
+namespace AOPT {
+
+
+    template<class MassSpringProblem>
+    double MassSpringSystemT<MassSpringProblem>::initial_system_energy() const{
+        if(msp_ != nullptr) {
+            Vec points = get_spring_graph_points();
+            return msp_.get()->eval_f(points);
+        }
+
+        return -1;
+    }
+
+    template<class MassSpringProblem>
+    void MassSpringSystemT<MassSpringProblem>::set_spring_graph_points(const Vec& _points) {
+        int n_vertices = sg_.n_vertices();
+
+        for(size_t i=0; i<n_vertices; ++i)
+            sg_.set_vertex(i, Point(_points[2*i], _points[2*i+1]));
+    }
+
+    template<class MassSpringProblem>
+    void MassSpringSystemT<MassSpringProblem>::save_spring_system(const char *_filename) const {
+        sg_.save_to_files(_filename);
+    }
+
+    template<class MassSpringProblem>
+    std::shared_ptr<MassSpringProblem> MassSpringSystemT<MassSpringProblem>::get_problem() const {
+        return msp_;
+    }
+
+    template<class MassSpringProblem>
+    void MassSpringSystemT<MassSpringProblem>::setup_problem(const int _spring_element_type, const bool _least_square) {
+        //set unknown variable number
+        n_unknowns_ = 2 * sg_.n_vertices();
+
+        //initialize the problem pointer
+        //for least square problem (Gauss-Newton)
+        if(_least_square) {
+            if(_spring_element_type == WITH_LENGTH){
+                msp_ = std::make_shared<MassSpringProblem>(sewlls_, n_unknowns_);
+            } else if(_spring_element_type == WITHOUT_LENGTH){
+                msp_ = std::make_shared<MassSpringProblem>(sels_, n_unknowns_);
+            }else {
+                std::cout << "Error: spring function index should be 0 or 1!" << std::endl;
+                return;
+            }
+        } else { //for normal problem
+            if (_spring_element_type == WITH_LENGTH) {
+                msp_ = std::make_shared<MassSpringProblem>(sewl_, n_unknowns_);
+            } else if (_spring_element_type == WITHOUT_LENGTH) {
+                msp_ = std::make_shared<MassSpringProblem>(se_, n_unknowns_);
+            } else if (_spring_element_type == WITH_LENGTH_PSD_HESS) {
+                msp_ = std::make_shared<MassSpringProblem>(element_wlen_psd_hess_, n_unknowns_);
+            } else {
+                std::cout << "Error: spring function index should be 0, 1 or 2!" << std::endl;
+                return;
+            }
+        }
+
+
+
+        //add spring elements
+        for (size_t i = 0; i < sg_.n_edges(); ++i)
+            msp_.get()->add_spring_element(sg_.from_vertex(i), sg_.to_vertex(i), sg_.coefficient(i), sg_.length(i));
+    }
+
+    template<class MassSpringProblem>
+    void MassSpringSystemT<MassSpringProblem>::add_constrained_spring_elements(const int _scenario) {
+        //------------------------------------------------------//
+        //Todo: add constrained spring elements to the problem
+        //implement both scenarios here.
+        
+        const double w(100000);
+        
+        if(_scenario == 1) {
+            msp_.get()->add_constrained_spring_element(get_grid_index(0,         0),         w, 0,         0);
+            msp_.get()->add_constrained_spring_element(get_grid_index(n_grid_x_, n_grid_y_), w, 2 * n_grid_x_, 2 * n_grid_y_);
+            msp_.get()->add_constrained_spring_element(get_grid_index(n_grid_x_, 0),         w, 2 * n_grid_x_, 0);
+            msp_.get()->add_constrained_spring_element(get_grid_index(0,         n_grid_y_), w, 0,         2 * n_grid_y_);
+        } else if (_scenario == 2){
+            for (int i = 0; i <= n_grid_x_; ++i) {
+                msp_.get()->add_constrained_spring_element(get_grid_index(i, 0),         w, i, 0);
+                msp_.get()->add_constrained_spring_element(get_grid_index(i, n_grid_y_), w, i, 2 * n_grid_y_);
+            }
+        }
+        //------------------------------------------------------//
+    }
+
+   
+
+
+
+    template<class MassSpringProblem>
+    void MassSpringSystemT<MassSpringProblem>::setup_linear_equality_constraints(SMat& _A, Vec& _b) const {
+        _A.resize(8, n_unknowns_);
+        _A.setZero();
+
+        _b.resize(8);
+        _b.setZero();
+
+        _A.insert(0, 2*get_grid_index(0, 0)  ) = 1; _b[0] = 0;
+        _A.insert(1, 2*get_grid_index(0, 0)+1) = 1; _b[1] = 0;
+
+        _A.insert(2, 2*get_grid_index(n_grid_x_, n_grid_y_)  ) = 1; _b[2] = 2 * n_grid_x_;
+        _A.insert(3, 2*get_grid_index(n_grid_x_, n_grid_y_)+1) = 1; _b[3] = 2 * n_grid_y_;
+
+        _A.insert(4, 2*get_grid_index(n_grid_x_, 0)  ) = 1; _b[4] = 2 * n_grid_x_;
+        _A.insert(5, 2*get_grid_index(n_grid_x_, 0)+1) = 1; _b[5] = 0;
+
+        _A.insert(6, 2*get_grid_index(0, n_grid_y_)  ) = 1; _b[6] = 0;
+        _A.insert(7, 2*get_grid_index(0, n_grid_y_)+1) = 1; _b[7] = 2 * n_grid_y_;
+    }
+
+
+    
+
+    template<class MassSpringProblem>
+    void MassSpringSystemT<MassSpringProblem>::setup_spring_graph() {
+        //------------------------------------------------------//
+        //TODO: set up the spring graph of n_grid_x by n_grid_y ()
+        //add vertices
+        for(int j = 0; j <= n_grid_y_; ++j)
+            for (int i = 0; i <= n_grid_x_; ++i)
+                sg_.add_vertex(Point(i, j));
+
+        //add edges
+        for(int j = 0; j < n_grid_y_; ++j) {
+            for(int i = 0; i < n_grid_x_; ++i) {
+                //horizontal edge
+                sg_.add_edge(get_grid_index(i, j), get_grid_index(i+1, j), 1., 1.);
+                //vertical edge
+                sg_.add_edge(get_grid_index(i, j), get_grid_index(i, j+1), 1., 1.);
+                //diagonal edge
+                sg_.add_edge(get_grid_index(i, j), get_grid_index(i+1, j+1), 1., sqrt(2.));
+                //diagonal edge
+                sg_.add_edge(get_grid_index(i+1, j), get_grid_index(i, j+1), 1., sqrt(2.));
+            }
+        }
+
+        //add right most
+        for(int j = 0; j < n_grid_y_; ++j)
+            sg_.add_edge(get_grid_index(n_grid_x_, j), get_grid_index(n_grid_x_, j+1), 1., 1.);
+
+        //add top cap
+        for(int i = 0; i < n_grid_x_; ++i)
+            sg_.add_edge(get_grid_index(i, n_grid_y_), get_grid_index(i+1, n_grid_y_), 1., 1.);
+        //------------------------------------------------------//
+    }
+
+    template<class MassSpringProblem>
+    typename MassSpringSystemT<MassSpringProblem>::Vec
+    MassSpringSystemT<MassSpringProblem>::get_spring_graph_points() const {
+        Vec points(n_unknowns_);
+        int n_vertices = sg_.n_vertices();
+
+        for(size_t i=0; i<n_vertices; ++i) {
+            points[2*i] = sg_.point(i)[0];
+            points[2*i+1] = sg_.point(i)[1];
+        }
+
+        return points;
+    }
+
+    template<class MassSpringProblem>
+    int MassSpringSystemT<MassSpringProblem>::get_grid_index(const int _i, const int _j) const {
+        assert(_i<=n_grid_x_ && _j<=n_grid_y_);
+        return (n_grid_x_+1)*_j + _i;
+    }
+
+    template<class MassSpringProblem>
+    size_t MassSpringSystemT<MassSpringProblem>::n_grid_points() const{
+        return (n_grid_x_+1) * (n_grid_y_+1);
+    }
+
+    template<class MassSpringProblem>
+    size_t MassSpringSystemT<MassSpringProblem>::n_edges() const{
+        return sg_.n_edges();
+    }
+}
---
+++ aopt-exercise8/include/Algorithms/NewtonMethods.hh
@@ -0,0 +1,321 @@
+#pragma once
+
+#include <FunctionBase/FunctionBaseSparse.hh>
+#include "LineSearch.hh"
+
+//== NAMESPACES ===============================================================
+
+namespace AOPT {
+    /**
+    * @brief NewtonMethods is just a list of functions implementing several variations of the
+    * newton's method */
+    class NewtonMethods {
+    public:
+        typedef FunctionBaseSparse::Vec Vec;   // dense vector arbitrary size
+        typedef FunctionBaseSparse::Mat Mat;   // dense matrix arbitrary size
+        typedef FunctionBaseSparse::T T;        //Triplets
+        typedef FunctionBaseSparse::SMat SMat;  // sparse matrix arbitrary size
+
+        /**
+         * @brief solve
+         * \param _problem pointer to any function/problem inheriting from FunctionBaseSparse
+         *        on which the basic Newton Method will be applied
+         * \param _initial_x starting point of the method
+         * \param _eps epsilon under which the method stops
+         * \param _max_iters maximum iteration of the method*/
+        static Vec solve(FunctionBaseSparse *_problem, const Vec& _initial_x, const double _eps = 1e-4, const int _max_iters = 1000000) {
+            std::cout << "******** Newton Method ********" << std::endl;
+
+            // squared epsilon for stopping criterion
+            double e2 = 2* _eps * _eps;
+
+            int n = _problem->n_unknowns();
+
+            // get starting point
+            Vec x = _initial_x;
+
+            // allocate gradient storage
+            Vec g(n);
+
+            // allocate hessian storage
+            SMat H(n, n);
+
+            // allocate search direction vector storage
+            Vec delta_x(n);
+            int iter(0);
+
+
+            Eigen::SimplicialLLT<SMat> solver;
+  
+            //------------------------------------------------------//
+            //TODO: implement Newton method
+            //fp is the function value of the previous iteration
+            double fp = std::numeric_limits<double>::max();
+
+            do {
+                ++iter;
+
+                // solve for search direction
+                _problem->eval_gradient(x, g);
+                _problem->eval_hessian(x, H);
+
+                // H dx = -g
+                solver.compute(H);
+                if(solver.info() == Eigen::NumericalIssue) {
+                    std::cerr << "Warning: LLT factorization has numerical issue!" << std::endl;
+                    break;
+                }
+
+                delta_x = solver.solve(-g);
+
+                // Newton decrement
+                double lambda2 = -g.transpose() * delta_x;
+
+                double f = _problem->eval_f(x);
+
+                // print status
+                std::cout << "iter: " << iter <<
+                          "   obj = " << f <<
+                          "   ||lambda||^2 = " << lambda2 << std::endl;
+
+                if (lambda2 <= e2 || fp <= f) break;
+
+
+
+                // step size
+                double t = LineSearch::backtracking_line_search(_problem, x, g, delta_x, 1.0);
+//            t = LineSearch::wolfe_line_search(_problem, x, g, delta_x, t);
+
+                // update
+                x += t * delta_x;
+                fp = f;
+
+            } while (iter < _max_iters);
+
+            //------------------------------------------------------//
+
+            return x;
+        }
+
+        /**
+         * @brief solve with the Projected Hessian method
+         * \param _problem pointer to any function/problem inheriting from FunctionBaseSparse.
+         *        This problem MUST provide a working eval_hession() function for this method to work.
+         *
+         * \param _initial_x starting point of the method
+         * \param _tau_factor the evolution factor of the tau coefficient
+         * \param _eps epsilon under which the method stops
+         * \param _max_iters maximum iteration of the method*/
+        static Vec solve_with_projected_hessian(FunctionBaseSparse *_problem, const Vec& _initial_x, const double _gamma = 10.0,
+                                                const double _eps = 1e-4, const int _max_iters = 1000000) {
+            bool converged = false;
+            return solve_with_projected_hessian(_problem, converged, _initial_x, _gamma, _eps, _max_iters);
+        }
+
+        static Vec solve_with_projected_hessian(FunctionBaseSparse *_problem, bool& _converged, const Vec& _initial_x, const double _gamma = 10.0,
+                                                const double _eps = 1e-4, const int _max_iters = 1000000) {
+            std::cout << "******** Newton Method with projected hessian ********" << std::endl;
+
+            // squared epsilon for stopping criterion
+            double e2 = 2*_eps * _eps;
+
+            int n = _problem->n_unknowns();
+
+            // get starting point
+            Vec x = _initial_x;
+
+            // allocate gradient storage
+            Vec g(n);
+
+            // allocate hessian storage
+            SMat H(n, n);
+
+            // allocate search direction vector storage
+            Vec delta_x(n);
+            int iter(0);
+
+            // identity and scalar to add positive values to the diagonal
+            SMat I(n, n);
+            I.setIdentity();
+
+            _converged = false;
+
+            Eigen::SimplicialLLT<SMat> solver;
+
+            //------------------------------------------------------//
+            //TODO: implement Newton with projected hessian method
+            //Hint: if the factorization fails, then add delta * I to the hessian.
+            //      repeat until factorization succeeds (make sure to update delta!)
+            double fp = std::numeric_limits<double>::max();
+
+            do {
+                ++iter;
+
+                // solve for search direction
+                _problem->eval_gradient(x, g);
+                _problem->eval_hessian(x, H);
+
+                int cnt = 0;
+                double delta = 0.;
+                
+                std::cout<<" H = "<<H<<std::endl;
+
+
+                solver.compute(H);
+                bool is_not_psd = solver.info() == Eigen::NumericalIssue;
+                std::cout<<" psd: "<<!is_not_psd<<std::endl;
+
+                while (is_not_psd && cnt < _max_iters) {
+                    if (cnt == 0) {
+                        delta = 1e-3 * std::abs(H.diagonal().sum()) / double(n);
+                    }
+                    H += delta * I;
+
+                    solver.compute(H);
+                    is_not_psd = solver.info() == Eigen::NumericalIssue;
+                    cnt++;
+                    delta *= _gamma;
+                }
+                delta_x = solver.solve(-g);
+
+                // Newton decrement
+                double lambda2 = g.transpose() * (-delta_x);
+
+                double f = _problem->eval_f(x);
+
+                // print status
+                std::cout << "iter: " << iter
+                          << "   obj = " << f
+                          << "   ||lambda||^2 = " << lambda2
+                          << "   n_projection_steps = " << cnt << std::endl;
+
+                if (lambda2 <= e2 || fp <= f) {
+                    _converged = true;
+                    break;
+                }
+
+                // step size
+                double t = LineSearch::backtracking_line_search(_problem, x, g, delta_x, 1.);
+
+                // update
+                x += t * delta_x;
+                fp = f;
+            } while (iter < _max_iters);
+
+            //------------------------------------------------------//
+
+
+            return x;
+        }
+
+
+
+        /**
+        * @brief solve problem with the linear equality constraints
+        * \param _problem pointer to any function/problem inheriting from FunctionBaseSparse.
+        *        This problem MUST provide a working eval_hession() function for this method to work.
+        *
+        * \param _initial_x starting point of the method
+        * \param _A the matrix of constraints Ax=b
+        * \param _b the vector of constraints Ax=b
+        * \param _eps epsilon under which the method stops
+        * \param _max_iters maximum iteration of the method*/
+        static Vec solve_equality_constrained(FunctionBaseSparse *_problem, const Vec& _initial_x, const SMat &_A, const Vec &_b,
+                                              const double _eps = 1e-4, const int _max_iters = 1000) {
+            std::cerr << "******** Equality Constrained Newton ********" << std::endl;
+
+            double eps2 = 2.0 *_eps * _eps;
+
+            // get number of unknowns
+            int n = _problem->n_unknowns();
+
+            // get number of constraints
+            int p = _A.rows();
+
+            // get starting point
+            Vec x = _initial_x;
+
+            // starting point satisfies constraints?
+            if ((_A * x - _b).squaredNorm() > eps2)
+                project_on_affine(x, _A, _b);
+
+            // allocate gradient storage
+            Vec g(n);
+            // allocate update storage
+            Vec dx(n);
+            // allocate hessian storage
+            SMat H(n, n);
+            // allocate KKT storage
+            SMat K(n + p, n + p);
+            // allocate rhs storage
+            Vec rhs(n + p);
+            // allocate solution storage
+            Vec dxl(n + p);
+
+            // count number of iterations
+            int iter(0);
+
+            Eigen::SparseLU<SMat> solver;
+            //------------------------------------------------------//
+            //TODO: implement the Newton with equality constraints
+            //Hint: the function to set up the KKT matrix is
+            //      provided below
+            
+            //------------------------------------------------------//
+
+
+            return x;
+        }
+
+        
+        
+        static void project_on_affine(Vec &_x, const SMat &_A, const Vec &_b) {
+            int n = _x.rows();
+            int p = _A.rows();
+
+            //------------------------------------------------------//
+            //TODO: project x to the hyperplane Ax = b
+            
+            
+            //------------------------------------------------------//
+
+            // check result
+            std::cerr << "Constraint violation after projection: " << (_A * _x - _b).squaredNorm() << std::endl;
+        }
+
+    private:
+        
+
+        static void setup_KKT_matrix(const SMat &_H, const SMat &_A, SMat& _K) {
+            const int n  = static_cast<int>(_H.cols());
+            const int m  = static_cast<int>(_A.rows());
+            const int nf = n+m;
+
+            // set up KKT matrix
+            // create sparse matrix
+            std::vector<T> trips;
+            trips.reserve(_H.nonZeros() + 2*_A.nonZeros());
+
+            // add elements of H
+            for (int k=0; k<_H.outerSize(); ++k)
+                for (SMat::InnerIterator it(_H,k); it; ++it)
+                    trips.emplace_back(static_cast<int>(it.row()),static_cast<int>(it.col()),it.value());
+
+            // add elements of _A
+            for (int k=0; k<_A.outerSize(); ++k)
+                for (SMat::InnerIterator it(_A,k); it; ++it)
+                {
+                    // insert _A block below
+                    trips.emplace_back(static_cast<int>(it.row())+n,static_cast<int>(it.col()),it.value());
+
+                    // insert _A^T block right
+                    trips.emplace_back(static_cast<int>(it.col()),static_cast<int>(it.row())+n,it.value());
+                }
+
+            // create KKT matrix
+            _K.resize(nf,nf);
+            _K.setFromTriplets(trips.begin(), trips.end());
+        }
+    };
+
+} // namespace AOPT
