e93459e3af54bba80f9efa697571241f0de7a33e
Alain Schaller <schallerala@gmail.com>
2021-12-11 12:49:08 +0100

    Initial commit of exercise 11

---
+++ aopt-exercise11/include/Algorithms/InteriorPoint.hh
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <FunctionBase/FunctionBaseSparse.hh>
+#include <Functions/InteriorPointProblem.hh>
+#include <Algorithms/NewtonMethods.hh>
+#include <Utils/OptimizationStatistic.hh>
+
+//== NAMESPACES ===============================================================
+
+namespace AOPT {
+
+    //== CLASS DEFINITION =========================================================
+    class InteriorPoint {
+    public:
+        // LA typedefs
+        using Vec = FunctionBaseSparse::Vec;
+
+        static Vec solve(FunctionBaseSparse *_obj, const Vec& _initial_x, const std::vector<FunctionBaseSparse*>& _constraints,
+                const double _eps = 1e-4, const double _mu = 10.0, const int _max_iters = 1000) {
+            std::cerr << "******** Interior Point ********" << std::endl;
+
+            // construct log-barrier problem
+            InteriorPointProblem problem(_obj, _constraints);
+            auto opt_st = std::make_unique<AOPT::OptimizationStatistic>(&problem);
+
+            // count number of iterations
+            int iter(0);
+
+            // current barrier parameter
+            double t = 1.0;
+
+            // number of constraints
+            double m = _constraints.size();
+
+            // points
+            Vec x = _initial_x;
+
+            //------------------------------------------------------//
+            //TODO: implement the interior point method
+            //Hint: Use projected newton method to solve for an approximated x.
+            
+            
+            //------------------------------------------------------//
+
+            opt_st->print_statistics();
+
+            return x;
+        }
+    };
+    //=============================================================================
+
+}
+
+
+
+
+
---
+++ aopt-exercise11/include/Functions/AreaConstraint2D.hh
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <FunctionBase/FunctionBaseSparse.hh>
+
+//== NAMESPACES ===================================================================
+
+namespace AOPT {
+
+    //== CLASS DEFINITION =========================================================
+    class AreaConstraint2D : public FunctionBaseSparse {
+    public:
+        // Area constraint: 1/2*det(v_01 | V02) >= eps
+        // f = -1/2*(x1 - x0)(y2 - y0) - (x2 - x0)(y1 - y0)) + eps <= 0
+        // constructor
+        AreaConstraint2D(const int _n, const int _idx0, const double _idx1, const double _idx2, const double _eps = 1e-10)
+                : FunctionBaseSparse(), n_(_n), idx0_(_idx0), idx1_(_idx1), idx2_(_idx2), eps_(_eps) {}
+
+        // number of unknowns
+        inline virtual int n_unknowns() override { return n_; }
+
+        // function evaluation
+        // _x stores the coordinates of all nodes
+        inline virtual double eval_f(const Vec &_x) override {
+            //------------------------------------------------------//
+            //Todo: implement the constraint function value
+            
+            
+            //------------------------------------------------------//
+        }
+
+        // gradient evaluation
+        inline virtual void eval_gradient(const Vec &_x, Vec &_g) override {
+            _g.setZero();
+            //------------------------------------------------------//
+            //Todo: implement the gradient and store in _g
+            
+            
+            //------------------------------------------------------//
+        }
+
+        // hessian matrix evaluation
+        inline virtual void eval_hessian(const Vec &_x, SMat &_h) override {
+            _h.setZero();
+            //------------------------------------------------------//
+            //Todo: implement the hessian matrix and store in _h
+            
+            //------------------------------------------------------//
+        }
+
+    private:
+        int n_;
+        // index of the nodes
+        int idx0_;
+        int idx1_;
+        int idx2_;
+
+        double eps_;
+    };
+
+//=============================================================================
+}
+
+
+
+
+
+
